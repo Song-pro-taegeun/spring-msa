aceb9022fdeaa3797bc48e65a94f1f32adf3819f
song-pro <xormsdlrnt@naver.com>
2025-12-24 11:33:23 +0900

    #5 user-service Consumer DLQ 설계, 설정 및 구현

---
+++ user-service/src/main/java/com/msa/user/config/kafka/KafkaConsumerConfig.java
@@ -0,0 +1,47 @@
+package com.msa.user.config.kafka;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+
+/**
+ * KafkaListener 동작 설정 클래스(컨테이너 레벨에서 정의)
+ * @KafkaListener 어노테이션이 붙은 메서드가 ack.acknowledge()를 호출해야만 offset이 커밋되도록 설정
+ */
+@Configuration
+@EnableKafka
+public class KafkaConsumerConfig {
+
+    /**
+     * Kafka Listener 컨테이너를 만들어준다
+     * @KafkaListener 하나당 내부적으로 Listener Container를 하나 만듦
+     */
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object>
+    kafkaListenerContainerFactory(
+            ConsumerFactory<String, Object> consumerFactory, // application-common에 정의한 컨슈머 설정을 그대로 사용
+            DefaultErrorHandler kafkaErrorHandler
+    ) {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+
+        // application-common에 정의한 컨슈머 설정을 그대로 사용
+        factory.setConsumerFactory(consumerFactory);
+
+        /**
+         * Listener 메서드 안에서 Acknowledgment.acknowledge()를 호출해야만 offset을 커밋
+         * offset 커밋을 개발자가 직접 통제
+         * type: MANUAL - 지연 커밋(poll 종료 시 offset 커밋)
+         */
+        factory.getContainerProperties()
+                .setAckMode(ContainerProperties.AckMode.MANUAL);
+
+        // Consumer Listener Exception 발생 시 kafkaErrorHandler 연계를 위함
+        factory.setCommonErrorHandler(kafkaErrorHandler);
+
+        return factory;
+    }
+}
--- user-service/src/main/java/com/msa/user/kafka/consumer/UserEventConsumer.java
+++ user-service/src/main/java/com/msa/user/kafka/consumer/UserEventConsumer.java
@@ -12,8 +12,9 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 @Component
 public class UserEventConsumer {
-    // user-created 토픽을 구독
-    // Consumer Group은 user-service
+    /**
+     * user-created 토픽을 구독
+     */
     @KafkaListener(
             topics = "user-created",
             groupId = "user-service"
@@ -26,23 +27,30 @@ public class UserEventConsumer {
             // tenantKey 기반 멀티테넌시 설정
             TenantContext.set(event.getTenantKey());
             log.info("event: {}", event);
+            int a = 1;
+            int b = 0;
+            int c = a/b;
 
-            // ack.acknowledge()를 하지 않으면 어떤 현상이 발생하는가?(offset commit이 되지 않았을 때)
-            // consumer 그룹은 구독하고 있는 토픽에서 발행한 end offset 만큼 Consumer 로직이 실행된다. (단, 특정 상황일 때,)
-            // 특정상황 1. Consumer가 재시작될 때
-            // 특정상황 2. 리밸런싱 발생 시
-            // 특정상황 3. 예외 발생
-            // 본인은 테스트를 위해 ack.acknowledge();를 주석처리하고, 강제 throw new RuntimeException(); 발생을 시킴
-            // current offset 다음부터 end offset까지 재실행하는 것을 확인함.
-            ack.acknowledge(); // 정상 처리 후 offset까지 Kafka에 커밋
+            /**
+             * ack.acknowledge()를 하지 않으면 어떤 현상이 발생하는가?(offset commit이 되지 않았을 때)
+             * consumer 그룹은 구독하고 있는 토픽에서 발행한 end offset 만큼 Consumer 로직이 실행된다. (단, 특정 상황일 때,)
+             * 특정상황 1. Consumer가 재시작될 때
+             * 특정상황 2. 리밸런싱 발생 시
+             * 특정상황 3. 예외 발생
+             * 본인은 테스트를 위해 0으로 값을 나눠 ArithmeticException를 발생ㅎ 시킴
+             * current offset 다음부터 end offset까지 재실행하는 것을 확인함.
+             */
+             ack.acknowledge(); // 정상 처리 후 offset까지 Kafka에 커밋
 
-            // Next Step!!!!
-            // Exception 발생 시 재시도 -> 실패 -> 재시도 -> 실패 등 무한 반복문에 빠지게 된다
-            // 이 메시지 하나로 파티션이 막히고, 정상 메시지 전부 처리를 하지 못하는 현상이 발생함.(Poison Message 라고 명명)
-            // ### DLQ(Dead Letter Queue)의 도입이 필요 - DLQ란 여러 번 처리에 실패한 메시지를 격리하는 kafka 토픽
-            // 예시 -> 재시도 3회 발생 시 DLQ로 이동(시스템 전체를 살리기위한 격리소!)
-            // DLQ는 "기존 topic"/ "partition, offset" / "exception 메시지", "stack trace" 정보로 구성되면 좋다
-            // DLQ는 Consumer 단위 이벤트 처리 실패 격리 용도
+            /**
+             * Next Step!!!!
+             * Exception 발생 시 재시도 -> 실패 -> 재시도 -> 실패 등 무한 반복문에 빠지게 된다
+             * 이 메시지 하나로 파티션이 막히고, 정상 메시지 전부 처리를 하지 못하는 현상이 발생함.(Poison Message 라고 명명)
+             * ### DLQ(Dead Letter Queue)의 도입이 필요 - DLQ란 여러 번 처리에 실패한 메시지를 격리하는 kafka 토픽
+             * 예시 -> 재시도 3회 발생 시 DLQ로 이동(시스템 전체를 살리기위한 격리소!)
+             * DLQ는 "기존 topic"/ "partition, offset" / "exception 메시지", "stack trace" 정보로 구성되면 좋다
+             * DLQ는 Consumer 단위 이벤트 처리 실패 격리 용도
+             */
         } catch (Exception e) {
             log.error("UserCreatedEvent 처리 실패: {}", event, e);
             throw e; // 재시도 유도

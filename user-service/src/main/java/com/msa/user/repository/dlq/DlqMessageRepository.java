d8d1c66026f3d4c79c887b8a84bc09cd3515bd5c
song-pro <xormsdlrnt@naver.com>
2026-08-20 18:29:52 +0900

    #32 User Service, Order Service DLQ 관리자 처리 프로세스(REPLAYING 원자적으로 변경)

--- user-service/src/main/java/com/msa/user/repository/dlq/DlqMessageRepository.java
+++ user-service/src/main/java/com/msa/user/repository/dlq/DlqMessageRepository.java
@@ -1,12 +1,26 @@
 package com.msa.user.repository.dlq;
 
 import com.msa.user.entity.dlq.DlqMessageEntity;
+import com.msa.user.entity.dlq.DlqStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.Optional;
 
 @Repository
 public interface DlqMessageRepository extends JpaRepository<DlqMessageEntity, Long> {
     Optional<DlqMessageEntity> findByOriginalTopicAndOriginalPartitionAndOriginalOffset(String originalTopic, Integer originalPartition, Long originalOffset);
+
+    @Modifying
+    @Query("""
+        UPDATE DlqMessageEntity d
+           SET d.status = :replaying
+         WHERE d.id = :id
+           AND d.status IN :replayableStatuses
+        """)
+    int markReplayingIfReplayable(@Param("id") Long id, @Param("replaying") DlqStatus replaying, @Param("replayableStatuses") Collection<DlqStatus> replayableStatuses);
 }

534a72529fd1d296e8dad3d9fa8850d3f1db800b
song-pro <xormsdlrnt@naver.com>
2026-08-20 16:30:27 +0900

    #29 Order Service - 제품 스냅샷 생성 시 원자적 업데이트 로직 구현

--- order-service/src/main/java/com/msa/order/service/order/OrderProductSnapshotSyncService.java
+++ order-service/src/main/java/com/msa/order/service/order/OrderProductSnapshotSyncService.java
@@ -5,12 +5,11 @@ import com.msa.common.kafka_event.TenantProductSnapshotPayload;
 import com.msa.order.entity.order.OrderProductSnapshot;
 import com.msa.order.repository.order.OrderProductSnapshotRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class OrderProductSnapshotSyncService {
@@ -20,22 +19,48 @@ public class OrderProductSnapshotSyncService {
     public void createProductSnapshot(TenantProductSnapshotEvent event){
         // 추가 작업 필요 내용
         // 멱등성 체크 로직 필요(inbox 도입)
-        // 이벤트 발행 시 updateVersion 추가 해당 컬럼 기록
 
-
-        List<OrderProductSnapshot> saveData = new ArrayList<>();
-        List<TenantProductSnapshotPayload> payloads = event.getPayloads();
-        payloads.forEach(data->{
-            OrderProductSnapshot obj = OrderProductSnapshot.create(
-                    data.getProductOptionId(),
-                    data.getProductId(),
-                    data.getProductName(),
-                    data.getOptionName(),
-                    data.getCurrency(),
-                    data.getPrice()
+        for (TenantProductSnapshotPayload payload : event.getPayloads()) {
+            // 다른 트랜잭션에서 동시에 처리되어 최종 이벤트 반영 순서가 꼬일 수 있으므로 조건부 업데이트 진행(원자적 갱신)
+            int updated = orderProductSnapshotRepository.updateIfNewer(
+                    payload.getProductOptionId(),
+                    payload.getProductId(),
+                    payload.getProductName(),
+                    payload.getOptionName(),
+                    payload.getCurrency(),
+                    payload.getPrice(),
+                    payload.getUpdateVersion()
             );
-            saveData.add(obj);
-        });
-        orderProductSnapshotRepository.saveAll(saveData);
+
+            // 업데이트 되지 않았을 때,
+            if (updated == 0) {
+                boolean exists = orderProductSnapshotRepository.existsById(payload.getProductOptionId());
+
+                // 제품이 없었다면, 기존 스냅샷이 없는 경우 최초 데이터를 저장
+                if (!exists) {
+                    orderProductSnapshotRepository.save(
+                            OrderProductSnapshot.create(
+                                    payload.getProductOptionId(),
+                                    payload.getProductId(),
+                                    payload.getProductName(),
+                                    payload.getOptionName(),
+                                    payload.getCurrency(),
+                                    payload.getPrice(),
+                                    payload.getUpdateVersion()
+                            )
+                    );
+                }
+                // 제품이 존재하는데, 업데이트가 되지 않았을 때
+                // 1. 동일 버전 이벤트가 다시 도착할 때(Inbox도입 후 중복으로 판단하여 처리 종료될 것임 참고)
+                // 2. 이전 버전 이벤트가 늦게 도착할 때
+                // 3. 다른 트랜잭션이 더 최신 버전을 반영할 때
+                else {
+                    log.debug("Order Service Product Snapshot 최신으로 반영된 제품이 존재. payload ={}", payload
+                    );
+                }
+            }
+        }
+
+        // Inbox 처리 완료 저장
     }
 }

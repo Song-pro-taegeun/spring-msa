6a77463be9f36066eb2a763f3351752cc3b657d9
song-pro <xormsdlrnt@naver.com>
2026-08-25 20:46:15 +0900

    #40 Order Service - redis bean 설정 파일 작성 및 원자적 재고 차감 스크립트, 결과 객체 구현

---
+++ order-service/src/main/java/com/msa/order/config/RedisScriptConfig.java
@@ -0,0 +1,35 @@
+package com.msa.order.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
+
+import java.util.List;
+
+/**
+ * lua script 리소스 파일 Bean 등록 설정파일
+ */
+@Configuration
+public class RedisScriptConfig {
+
+    // redis 재고 초기화 script Bean
+    @Bean
+    public RedisScript<List> reserveInventoryScript() {
+        return createScript(
+                "redis/scripts/inventory_reserve.lua",
+                List.class
+        );
+    }
+
+    private <T> RedisScript<T> createScript(
+            String path,
+            Class<T> resultType
+    ) {
+        DefaultRedisScript<T> script = new DefaultRedisScript<>();
+        script.setLocation(new ClassPathResource(path));
+        script.setResultType(resultType);
+        return script;
+    }
+}
